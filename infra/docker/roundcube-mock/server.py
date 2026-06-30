7c44dae0983f7721ae641b926b5dc96fbbf304f8
Florian Schaal <florian.schaal@protonmail.com>
2026-06-30 19:31:58 +0200

    feat(CVE-2023-43770): add detection feed and test infrastructure
    - Feed YAML with Roundcube version fingerprint via login page (QoD 50)
    - Python/Docker mock (vuln v1.6.2 + patched v1.6.3), HTTP:80 + HTTPS:443
    - Ansible inventory + playbook, 4 test cases (TP×2 + TN×2)
    
    Closes #121

--- infra/docker/roundcube-mock/server.py
+++ infra/docker/roundcube-mock/server.py
@@ -3,24 +3,21 @@ import os, ssl, threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 VULN_MODE = os.environ.get("ROUNDCUBE_MODE", "patched") == "vuln"
+VERSION = "1.6.2" if VULN_MODE else "1.6.3"
 
-# 1609 = version 1.6.9 (< 1.6.11, vulnerable)
-# 1611 = version 1.6.11 (patched)
-RCVERSION = "1609" if VULN_MODE else "1611"
+LOGIN_HTML = """<html><head><title>Roundcube Webmail :: Welcome to Roundcube Webmail</title>
+<script>var rcversion='{v}', rcube_server_version='{v}';</script>
+</head><body>Login to Roundcube...</body></html>""".format(v=VERSION)
 
-HOMEPAGE = (
-    "<!DOCTYPE html><html><head><title>Roundcube Webmail</title></head><body>"
-    "<script>var rcmail_config = {\"rcversion\":RCVERSION_PLACEHOLDER, "
-    "\"skin\":\"elastic\", \"standard_windows\":false};</script>"
-    "<div id=\"loading\">Loading Roundcube...</div>"
-    "</body></html>"
-).replace("RCVERSION_PLACEHOLDER", RCVERSION)
+APP_JS = "/* Roundcube Webmail v{v} */\nvar rcmail = null;\n".format(v=VERSION)
+
 
 class Handler(BaseHTTPRequestHandler):
     def log_message(self, *args): pass
 
     def _send(self, code, body, ct="text/html"):
-        if isinstance(body, str): body = body.encode()
+        if isinstance(body, str):
+            body = body.encode()
         self.send_response(code)
         self.send_header("Content-Type", ct)
         self.send_header("Content-Length", str(len(body)))
@@ -28,15 +25,16 @@ class Handler(BaseHTTPRequestHandler):
         self.wfile.write(body)
 
     def do_GET(self):
-        if self.path in ("/", "/?_task=login"):
-            self._send(200, HOMEPAGE)
+        if self.path.startswith("/?_task=login") or self.path == "/":
+            self._send(200, LOGIN_HTML)
+        elif self.path == "/program/js/app.js":
+            self._send(200, APP_JS, "application/javascript")
         else:
-            self._send(404, "Not found")
+            self._send(404, "Not found", "text/plain")
 
     def do_POST(self):
-        length = int(self.headers.get("Content-Length", 0))
-        self.rfile.read(length)
-        self._send(302, b"", ct="text/plain")
+        self.do_GET()
+
 
 def _make_https_server(port):
     ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
@@ -44,6 +42,7 @@ def _make_https_server(port):
     srv = HTTPServer(("0.0.0.0", port), Handler)
     srv.socket = ctx.wrap_socket(srv.socket, server_side=True)
     return srv
+
 
 if __name__ == "__main__":
     https = _make_https_server(443)
